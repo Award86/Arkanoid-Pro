fd6e25235dcd49f84319f1ce2fe8da489ef4c4e2
Award86 <v.kusmyna@gmail.com>
2025-01-30 11:12:06 +0200

    Update skill_system.py

--- game/systems/skill_system.py
+++ game/systems/skill_system.py
@@ -3,22 +3,21 @@ from kivy.properties import DictProperty, NumericProperty
 class SkillSystem:
     xp = NumericProperty(0)
     level = NumericProperty(1)
-    skill_tree = DictProperty({
-        'paddle_size': {'level': 0, 'max': 5, 'xp_cost': 100},
-        'ball_speed': {'level': 0, 'max': 3, 'xp_cost': 150},
-        'magnet': {'level': 0, 'max': 2, 'xp_cost': 200}
+    skills = DictProperty({
+        'paddle_size': {'current': 0, 'max': 5, 'cost': 100},
+        'ball_speed': {'current': 0, 'max': 3, 'cost': 150},
+        'extra_life': {'current': 0, 'max': 2, 'cost': 200}
     })
-    
+
     def add_xp(self, amount):
         self.xp += amount
         while self.xp >= self.required_xp:
             self.level_up()
-            
+
     @property
     def required_xp(self):
         return self.level * 500 + 1000
-            
+
     def level_up(self):
         self.level += 1
         self.xp -= self.required_xp
-        # Розблокування нових можливостей
