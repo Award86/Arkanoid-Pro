b368dbaad585ad91984b6ae170b0409ce6d066d6
Award86 <v.kusmyna@gmail.com>
2025-01-30 10:57:11 +0200

    Update game_engine.py

--- game/core/game_engine.py
+++ game/core/game_engine.py
@@ -1,15 +1,23 @@
 from kivy.clock import Clock
-from kivy.vector import Vector
+from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ObjectProperty
 
-class GameEngine:
-    player_score = NumericProperty(0)
+class GameEngine(Widget):
     current_level = NumericProperty(1)
+    player_score = NumericProperty(0)
+    game_speed = NumericProperty(1.0)
     
-    def __init__(self):
-        self.physics_engine = PhysicsSystem()
-        self.save_system = SaveManager()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.skill_system = SkillSystem()
+        self.physics = PhysicsEngine()
+        self._game_loop = Clock.schedule_interval(self.update, 1/60.)
         
-    def load_level(self, level_num):
-        # Логіка завантаження рівня
+    def update(self, dt):
+        self.physics.update_objects(dt * self.game_speed)
+        self.check_collisions()
+        self.check_game_state()
+        
+    def check_collisions(self):
+        # Детектування зіткнень між всіма об'єктами
         pass
